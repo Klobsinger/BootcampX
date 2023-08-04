ff56689e677eb71d46588598bd9a1a54a5a554b8
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-08-04 15:00:35 -0400

    feat: students.js & teachers.js Implement sql querys to fetch student and teacher info student.js using basic args commands to query student names, ids, and cohort name teachers.js using basic args commands to query teachers names who made assistance requests for a cohort

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const args = process.argv.slice(2);
+const cohortSelect = args[0]
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${cohortSelect}%'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+`)
+.then(res => {
+  console.log('connected')
+  res.rows.forEach(row => {
+    console.log(`${row.cohort} ${row.teacher}`);
+  })
+  pool.end()
+});
